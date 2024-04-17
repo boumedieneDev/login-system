34abbe981549b44ec5135e356f952d47cea319af
boumedieneDev <114975635+boumedieneDev@users.noreply.github.com>
2024-04-17 02:10:48 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="website/style.css" />
+    <link rel="stylesheet" href="website/style1.css" />
+</head>
+<body>
+    <header>
+        <div class="mynavbar">
+            <div class="logo"><a href="../index.html"><img src="C:\Users\dagpb\OneDrive\Documents\flutter\PROJET\LOGO.png" alt="ecrilli" width="90px" height="100px"></a></div>
+            <ul class="links">
+                <li class="navbar-li"><a class="navbar-a" href="index.html">Home</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">Cours</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">Research</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">News</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="about.html">About</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="contact.html">Contact</a></li>
+            </ul>
+            <div class="auth-buttons">
+                <!-- These buttons will be toggled based on user login status -->
+                <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+               <a href="logout.php">Logout</a>
+            </div>
+            <div class="toggle-btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        <div class="mydropdown-menu">
+            <ul class="dropdown-links">
+                <li class="navbar-li"><a class="navbar-a" href="index.html">Home</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">Cours</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">Research</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="ourcars.html">News</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="about.html">About</a></li>
+                <li class="navbar-li"><a class="navbar-a" href="contact.html">Contact</a></li>
+            </ul>
+            <div class="auth-buttons">
+                <!-- These buttons will be toggled based on user login status -->
+                <button class="login-btn">Login</button>
+                <button class="signup-btn">Signup</button>
+                <button class="account-btn" style="display: none;">Account</button>
+            </div>
+        </div>
+    </header>
+    <section class="hero">
+        <div class="hero-container">
+          <div class="column-left">
+            <h1>Building the Future</h1>
+            <h4>Learn, innovate, create.</h4>
+            <h6> Journey Starts in Computer Science departement.</h6>    
+            <button>Get Started</button>
+          </div>
+        <div class="column-right">
+            <img
+              src="programming.webp"
+              class="hero-image"
+            />
+        </div>
+      </section>
+      <section class="hero1">
+        <div class="hero-container1">
+          
+        <div class="column-left1">
+            <img
+              src="programming.webp"
+              class="hero-image"
+            />
+        </div>
+        <div class="column-right1">
+            <h1>Your modules are HERE</h1>
+            <h4>Module like OOP ,Algorithm,Cyber Security.</h4>
+               
+            <button>modules</button>
+          </div>
+      </section>
+    
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
